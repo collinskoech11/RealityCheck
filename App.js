c48e29d53f7d71aa20d7987ee067b5b517a17325
Collins Koech <collinskoechck34@gmail.com>
2020-10-16 10:12:41 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'App.css';
+import Header from './Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <StatusBar/>
+      <Text/>
     </View>
   );
 }
---
+++ Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div class="Main">
+            This is the App Header
+        </div>
+    )
+}
